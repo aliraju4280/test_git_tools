5b12bb911bd1cfcce879ef1dc34e050410a44f66
aliraju4280 <56260845+aliraju4280@users.noreply.github.com>
2019-10-19 22:57:08 +0530

    Update Wordcount.scala

--- Wordcount.scala
+++ Wordcount.scala
@@ -5,19 +5,23 @@ import scala.math.random
 import org.apache.spark.sql.SparkSession
 
 object Wordcount {
+	
 	def main(args: Array[String]) {
 	  
 		if (args.length < 2) {
       System.err.println("Usage: JavaWordCount <Input-File> <Output-File>");
       System.exit(1);
+			
     }
 		
 		val spark = SparkSession //session created
+		
 				.builder
 				.appName("Wordcount")
 				.getOrCreate()
 
     val data = spark.read.textFile(args(0)).rdd // tesxt file added
+		
     
     val result = data.flatMap { line => {
       line.split(" ")
